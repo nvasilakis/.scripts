d3827687665d351628af739ecf08b4d221930d49
nvasilakis <nikos.ailo@gmail.com>
2011-09-23 16:47:14 -0400

    Adding scripts on skype notification

---
+++ skype-dbus-service.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python
+#
+#Copyright 2009, 2010, Ned Hoy
+#
+#Authors:
+#    Ned Hoy <nedhoy@gmail.com>
+#    Modified heavily by not_insane so it works like it should!
+#
+#   much code taken from Highly Ubuntu 9.04 Intergrated GMail Notifier https://launchpad.net/gm-notify
+#   and the interwebs
+#
+
+# Most notifications are handled in skype-notify.py
+# However, the dbus service handles these notifications:
+# ChatIncomingInitial   :   skype-name, skype-message
+# ChatIncoming          :   skype-name, skype-message
+# ContactOnline         :   skype-name, no code for indicator yet
+
+import dbus, dbus.glib, dbus.service
+import gobject
+import gtk
+
+import indicate
+import pynotify
+
+import wnck
+
+from time import time
+import time
+
+import os
+import re
+import sys
+from threading import Thread
+
+class Service (dbus.service.Object):
+    '''D-Bus server for receiving notifications'''
+    # see http://dbus.freedesktop.org/doc/dbus-python/doc/tutorial.html#data-types
+    # def __init__(self):
+    @dbus.service.method(dbus_interface='org.skypenotify.Interface',
+                         in_signature='sss', out_signature='')
+    def notify (self, event, skypename, skypemessage):
+        
+        print("{contact} says '{message}'".format(contact=skypename, message=skypemessage))
+        remote_bus = dbus.SessionBus()
+        out_connection = remote_bus.get_object('com.Skype.API', '/com/Skype')
+        out_connection.Invoke('NAME mySkypeController')
+        out_connection.Invoke('PROTOCOL 5')
+        fullname = ' '.join(out_connection.Invoke('GET USER ' + skypename + ' FULLNAME').split()[3:])
+        f = open("/home/nikos/Desktop/dbg.txt", "a")
+        f.write("test1")
+        f.close()
+        if 'ChatIncoming' in event:
+            if not isConversationActive(fullname):
+                #if not isIndicated(skypename):
+                #    addIndicator(event, skypename)
+                #n = pynotify.Notification(skypename, skypemessage, "skype")
+                #n.set_hint_string('append', 'allowed')
+                #n.show()
+                showNotification(skypename, "WA!", "skype", False)
+                if not isIndicated(skypename):
+                    mm_source = indicate.Indicator()
+                    mm_source.set_property("subtype", "im")
+                    mm_source.set_property("sender", skypename)
+                    mm_source.set_property("draw-attention", "true")
+                    mm_source.set_property_time("time", time.time())
+                    mm_source.connect("user-display", display_cb)
+                    mm_source.show()
+                    indicators.append(mm_source)
+                    contactsIndicated.append(skypename)
+                    contactNames.append(fullname)
+        elif event == 'ContactOnline':
+            if not findChatWindow(skypename).is_active():
+                showNotification(skypename, "is online", "skype",  False)
+
+
+indicators = []
+contactsIndicated = []
+contactNames = []
+
+#def timeout_cb(indicator):
+#    '''timeout example'''
+#    print "Modifying properties"
+#    global lastpath
+#    indicator.set_property_time("time", time())
+#    if lastpath == PATHA:
+#        lastpath = PATHB
+#    else:
+#        lastpath = PATHA
+#
+#    pixbuf = gtk.gdk.pixbuf_new_from_file(lastpath)
+#
+#    indicator.set_property_icon("icon", pixbuf)
+#
+#    return True
+
+def isIndicated(contact):
+    if contact in contactsIndicated: return True
+
+def isConversationActive(contact):
+    '''Check if conversation with contact is active (has focus)'''
+    for window in scr.get_windows():
+        if isConversationWithContact(window, contact) and window.is_active():
+            return True
+    return False
+
+def isConversationWithContact(window, contact):
+    '''Checks if given window is a conversation with a particular contact'''
+    #FOR LOLS: '''Checks if a window is a skype window instance of a chat with a contact'''
+    return 'Skype' in window.get_application().get_name() and \
+        'Chat' in window.get_name() and contact in window.get_name()
+
+def findChatWindow(contact):
+    '''Finds the chat window waiting for attention since the notification'''
+    for window in scr.get_windows():
+        #print ("-------")
+        #if window.get_name():
+        #    print(window.get_name())
+        #    print(window.get_application().get_name())
+        if isConversationWithContact(window, contact):
+            return window
+
+def display_cb(indicator, tempfixme):
+    ''' The indicator has been clicked: Focus skype chat window '''
+    #FIXME TypeError: display_cb() takes exactly 1 argument (2 given)
+    # probably a change in libindicate
+    #TODO match with the contact's skype username instead of alias (incase you rename them or something)
+    contact = indicator.get_property("sender")
+
+    remote_bus = dbus.SessionBus()
+    out_connection = remote_bus.get_object('com.Skype.API', '/com/Skype')
+    out_connection.Invoke('NAME mySkypeController')
+    out_connection.Invoke('PROTOCOL 5')
+    chatsRecent = out_connection.Invoke('SEARCH MISSEDCHATS')[6:].split(", ")
+    chatsRecentMembers = []
+
+    for x in chatsRecent:
+        i = len(chatsRecentMembers)
+        chatsRecentMembers.append([])
+        memobjs = out_connection.Invoke('GET CHAT ' + x + ' MEMBEROBJECTS')
+        chatsRecentMembers[i] = ' '.join(memobjs.split()[3:]).split(", ")
+        for j in range(0, len(chatsRecentMembers[i])):
+            print out_connection.Invoke('GET CHATMEMBER ' + chatsRecentMembers[i][j] + ' IDENTITY').split()
+            chatsRecentMembers[i][j] = out_connection.Invoke('GET CHATMEMBER ' + chatsRecentMembers[i][j] + ' IDENTITY').split()[3]
+        if x.find(contact) is not -1 and memobjs.strip() == '':
+            out_connection.Invoke('OPEN CHAT ' + x)
+            break
+    print chatsRecent
+    print chatsRecentMembers
+    print contact
+
+    for x in range(0, len(chatsRecentMembers)):
+        for y in chatsRecentMembers[x]:
+            if y.find(contact) is not -1:
+                out_connection.Invoke('OPEN CHAT ' + chatsRecent[x])
+
+    try:
+        window = findChatWindow(contact)
+        window.activate(int(time.time()))
+    except:
+        print ""
+
+    for i in range(0, len(contactsIndicated)):
+        try:
+            if contactsIndicated[i] == contact:
+                indicators[i].hide()
+                indicators.remove(indicators[i])
+                contactsIndicated.remove(contactsIndicated[i])
+                contactNames.remove(contactNames[i])
+                break
+        except:
+            print ""
+
+def server_display_cb(server, tempfixme):
+    '''The server indicator has been clicked: focus Skype main window'''
+    #FIXME TypeError: server_display_cb() takes exactly 1 argument (2 given)
+    # probably a change in libindicate
+    print server
+    #Check each window to see if it is the Skype contact list and give it focus
+    map(focus_if_contact_list, scr.get_windows())
+    try:
+        # Try and set skype window to normal
+        remote_bus = dbus.SessionBus()
+        out_connection = remote_bus.get_object('com.Skype.API', '/com/Skype')
+        out_connection.Invoke('NAME mySkypeController')
+        out_connection.Invoke('PROTOCOL 5')
+        out_connection.Invoke('SET WINDOWSTATE NORMAL')
+        out_connection.Invoke('FOCUS')
+    except:
+        os.system("skype&")
+
+def on_state_change(window, changed_mask, new_state):
+    '''handle the chat becoming focused or closing by removing the indicator'''
+    #FIXME remove only the relevant indicator.
+
+    print("***state-changed***")
+    print("window still needs attention? ", window.needs_attention ())
+
+    if not window.needs_attention ():
+        removeIndicator()
+
+def focus_if_contact_list(window):
+    '''Check if particular window is the Skype contact list and give focus'''
+    #print ("--------")
+    #if window.get_name():
+    #    print(window.get_name())
+    #    app = window.get_application()
+    #    print(app.get_name())
+
+    #FIXME there are some windows that are neither the contact list nor chats,
+    #such as voice calls, that we also need to filter out. Would be best to use
+    #the skype login name because it appears on the contact list
+    if ("Skype" in window.get_application().get_name()
+         and "Chat" not in window.get_name()):
+        if not window.is_active():
+            window.activate(int(time.time()))
+
+def showNotification(summary, message, icon, append):
+    '''Display a notification with a title summary, message body and icon'''
+    global n
+    n = pynotify.Notification(summary, message, icon)
+    if append:
+        print "append"
+    n.set_hint_string('append', 'allowed')
+    n.show()
+
+def addIndicator(event, contact):
+    '''adds a indicator to the indicator-applet which results in the "unread counter"
+    increased by one. The indicator is stored in a class-list to keep the reference.
+    If you delete this the indicator will be removed from the applet, too'''
+    pixbuf = gtk.gdk.pixbuf_new_from_file("/usr/share/icons/skype.png")
+
+    #setup indicator
+    indicator = indicate.Indicator() # changed as suggested by mirko_3 http://ubuntuforums.org/showpost.php?p=8094319&postcount=42
+    indicator.set_property("subtype", "im")
+    indicator.set_property("sender", contact)
+    #indicator.set_property_time("time", time())
+    indicator.set_property_icon("icon", pixbuf)
+
+    indicator.connect("user-display", display_cb)
+
+    indicator.show()
+
+    indicators.append(indicator) #not sure about this - if it isn't done: no indicator O.o
+    contactsIndicated.append(contact)
+
+    window = findChatWindow(contact)
+    window.connect("state-changed", on_state_change)
+
+def removeIndicator():
+    '''remove an indicator - atm it removes all indicators FIXME'''
+    global indicators
+    global contactsIndicated
+    global contactNames
+    indicators = []
+    contactsIndicated = []
+    contactNames = []
+
+def dummy():
+    ''' Create one indicator and instantly delete it to make the server show up in the applet (HACK?)'''
+    indicator = indicate.Indicator()
+    indicator.set_property("subtype", "im")
+    indicator.set_property("sender", "dummy")
+    indicator.show()
+
+class cleanupthread(Thread):
+    def run(self):
+        while 1:
+            for i in range(0, len(contactNames)):
+                try:
+                    if findChatWindow(contactNames[i]).is_active():
+                        indicators[i].hide()
+                        indicators.remove(indicators[i])
+                        contactsIndicated.remove(contactsIndicated[i])
+                        contactNames.remove(contactNames[i])
+                        break
+                except:
+                    print ""
+            time.sleep(0.1)
+cleanthread = cleanupthread()
+cleanthread.daemon = True
+cleanthread.start()
+
+if __name__ == "__main__":
+    # Initiate pynotify
+    if not pynotify.init("Skype Hack"):
+        sys.exit(-1)
+    # Setup indicate server
+    server = indicate.indicate_server_ref_default()
+    server.set_type("message.im")
+    server.set_desktop_file("/usr/share/skype-notify-improved/skype.desktop")
+#    server.set_desktop_file("/usr/share/applications/skype.desktop")
+    server.connect("server-display", server_display_cb)
+
+    server.show()
+
+    # Get list of windows with wink
+    scr = wnck.screen_get_default()
+    server_display_cb("startup", 1)
+    while gtk.events_pending():
+	    gtk.main_iteration()
+
+    # Export notify method over dbus
+    bus = dbus.SessionBus()
+    name = dbus.service.BusName('org.skypenotify.Service', bus=bus)
+    obj = Service(name, '/')
+    loop = gobject.MainLoop()
+    print('Listening')
+    gobject.threads_init()
+    context = loop.get_context()
+    
+    while 1:
+        time.sleep(0)
+        context.iteration(True)
---
+++ skype-notify.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+#    Python script to make Skype use notify-osd
+
+# Copyright (c) 2009, 2010, Lightbreeze
+
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# to use this script: Open Skype -> Open the menu and press 'Options' or press Ctrl-O
+# -> hit the 'Advanced' button and check 'Execute the following script on _any_ event'
+# -> paste: python /path/to/skype-notify.py -e"%type" -n"%sname" -f"%fname" -p"%fpath" -m"%smessage" -s%fsize -u%sskype
+# -> disable or enable the notifications you want to receive.
+
+import sys
+from optparse import OptionParser
+
+import pynotify
+import dbus, dbus.glib
+
+class NotifyForSkype:
+    def __init__(self):
+        # Initiate pynotify
+        if not pynotify.init("Skype Notifier"):
+            sys.exit(-1)
+
+        # Add argument parser options
+        parser = OptionParser()
+        parser.add_option("-e", "--event", dest="type", help="type of SKYPE_EVENT")
+        parser.add_option("-n", "--sname", dest="sname", help="display-name of contact")
+        parser.add_option("-u", "--skype", dest="sskype", help="skype-username of contact")
+        parser.add_option("-m", "--smessage", dest="smessage", help="message body", metavar="FILE")
+        parser.add_option("-p", "--path", dest="fpath", help="path to file")
+        parser.add_option("-s", "--size", dest="fsize", help="incoming file size")
+        parser.add_option("-f", "--filename", dest="fname", help="file name", metavar="FILE")
+        (self.o, args) = parser.parse_args()
+
+        #print(args)
+        #print(sys.argv)
+        #print(self.o.type)
+
+        self.handleNotification()
+
+    def handleNotification(self):
+        notification = self.o.type
+        #FIXME this should run method based on the string
+        # and not have to use all these else-ifs
+
+        if notification == 'SkypeLogin': self._SkypeLogin()
+        elif notification == 'SkypeLogout': self._SkypeLogout()
+        elif notification == 'SkypeLoginFailed': self._SkypeLoginFailed()
+        #elif notification == 'CallConnecting': self._CallConnecting()
+        #elif notification == 'CallRingingIn': self._CallRingingIn()
+        #elif notification == 'CallRingingOut': self._CallRingingOut()
+        elif notification == 'CallAnswered': self._CallAnswered()
+        elif notification == 'VoicemailReceived': self._VoicemailRecieved()
+        elif notification == 'VoicemailSent': self._VoicemailSent()
+        elif notification == 'ContactOnline': self._ContactOnline()
+        #elif notification == 'ContactOffline': self._ContactOffline()
+        elif notification== 'ContactDeleted': self._ContactDeleted()
+        elif notification == 'ChatIncomingInitial': self._ChatIncomingInitial()
+        elif notification == 'ChatIncoming': self._ChatIncoming()
+        #elif notification == 'ChatOutgoing': self._ChatOutgoing()
+        elif notification == 'ChatJoined': self._ChatJoined()
+        elif notification == 'ChatParted': self._ChatParted()
+        elif notification == 'TransferComplete': self._TransferComplete()
+        elif notification == 'TransferFailed': self._TransferFailed()
+        elif notification == 'Birthday': self._Birthday()
+        # event handling for SMS notifications
+        elif notification == 'SMSSent': self._SMSSent()
+        elif notification == 'SMSFailed': self._SMSFailed()
+
+    def _SkypeLogin(self):
+        self.showNotification("Skype","You have logged into Skype with {contact}".format(contact=self.o.sname),"skype")
+
+    def _SkypeLogout(self):
+        pass
+
+    def _SkypeLoginFailed(self):
+        self.showNotification("Skype login failed",None,"user-offline")
+
+    def _CallConnecting(self):
+        self.showNotification("Dialing... {contact}".format(contact=self.o.sname),None,"skype")
+        #some of these should be merged and update to the same bubble: Call Connecting -> CallRingingOut -> Call Answered
+
+    def _CallRingingOut(self):
+        self.showNotification("Calling {contact}".format(contact=self.o.sname),"skype")
+
+    def _CallAnswered(self):
+        self.showNotification("Call Answered",None,"skype")
+
+    def _CallRingingIn(self):
+        self.showNotification(self.o.sname,"is calling you","skype")
+
+    def _VoicemailRecieved(self):
+        self.showNotification(self.o.sname,"Voicemail Received","skype")
+
+    def _VoicemailSent(self):
+        self.showNotification("Voicemail Sent",None,"skype")
+
+    def _ContactOnline(self):
+        self.showNotification(self.o.sname,"is online","skype")
+        #self.addIndicator(self.o.type, self.o.sname, self.o.smessage)
+
+    def _ContactOffline(self):
+        self.showNotification(self.o.sname,"is offline","skype")
+
+    def _ContactDeleted(self):
+        self.showNotification("Contact Deleted", "{contact} has been deleted from your contact list".format(contact=self.o.sname),"skype")
+
+    def _ChatIncomingInitial(self):
+        if not (self.addIndicator(self.o.type, self.o.sname, self.o.smessage)):
+            #FIXME this notification should be shown if the dbus-service is not running
+            self.showNotification(self.o.sname, self.o.smessage, "notification-message-IM")
+
+    def _ChatIncoming(self):
+        self._ChatIncomingInitial()
+
+    def _ChatOutgoing(self):
+        self.showNotification(self.o.sname,self.o.smessage,"notification-message-IM")
+
+    def _ChatJoined(self):
+        self.showNotification("{contact} joined chat".format(contact=self.o.sname),self.o.smessage,"emblem-people")
+
+    def _ChatParted(self):
+        self.showNotification("{contact} left chat".format(contact=self.o.sname),self.o.smessage,"emblem-people")
+
+    def _TransferComplete(self):
+        self.showNotification("Transfer Complete","{path}/{filename}".format(filename=self.o.fname,path=self.o.fpath),"gtk-save")
+        #TODO show dialog [ok] [open file] [reveal in folder]
+        # or preview in gloobus :D if suitable format
+
+    def _TransferFailed(self):
+        self.showNotification("Transfer Failed","{filename}".format(filename=self.o.fname),"error")
+
+    def _Birthday(self):
+        self.showNotification(self.o.sname,"has a birthday Tomorrow","appointment-soon")
+
+    def _SMSSent(self):
+        self.showNotification(self.o.sname,"SMS sent","skype")
+
+    def _SMSFailed(self):
+        self.showNotification(self.o.sname,"SMS failed","error")
+
+    def showNotification(self, summary, message, ikon):
+        '''takes a title summary a message, and an icon to display the notification. Returns the created notification object'''
+        if summary == None: summary = " "
+        n = pynotify.Notification(summary, message, ikon)
+        n.set_hint_string('append', 'allowed')
+        n.show()
+        return n
+
+    def addIndicator(self, event, sname, smessage):
+        '''send event type, contact's display name, and message text over D-Bus'''
+        bus = dbus.SessionBus()
+        try:
+           #self.showNotification("1","2","skype")
+           server = dbus.Interface(bus.get_object('org.skypenotify.Service', '/'), 'org.skypenotify.Interface')
+           server.notify(event, sname, smessage)
+        except dbus.exceptions.DBusException:
+            print >> sys.stderr, "You should be running skype-dbus-service.py for messaging menu support and to append new text in notification bubbles"
+            return False
+        print("connected to D-Bus org.skypenotify.Service server")
+        return True
+
+cm = NotifyForSkype()
+sys.exit(-1) #hack TODO make sure this program quits properly
