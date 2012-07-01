0653607cec63109a5a218e2f574342681f23536c
nvasilakis <nikos.ailo@gmail.com>
2012-07-01 09:18:13 -0400

    Add insomnia.sh -- for sleeping with music

---
+++ insomnia.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+  
+##
+# 2012, Nikos Vasilakis
+# n.c.vasilakis@gmail.com
+#
+# A script that helps me sleep by shutting down media player
+#
+# Usage: ./insomnia.sh
+##
+
+# Output current settings
+echo "Current Volume Settings:";
+amixer get Master | grep Mono ;
+period=$1;
+half=$(($period/2));
+echo "sleeping for $half x2";
+sleep $period;
+
+# Lowering volume slowly
+for (( i = 0; i < 100; i++ )); do
+  sleep 5;
+  amixer sset Master,0 1- > /dev/null
+done
+
