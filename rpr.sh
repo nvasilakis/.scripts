359aff0f0e6bc5c6099d63de18a78c10e2d5c116
Nikos Vasilakis <nikos.ailo@gmail.com>
2013-04-18 03:38:15 -0400

    Add a print script

---
+++ rpr.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+  
+##
+# 2013, Nikos Vasilakis
+# n.c.vasilakis@gmail.com
+#
+# A script that sends files to remote printers
+#
+# Usage: ./rpr.sh <file>
+##
+
+f="$1"
+echo "Sending $f for printing on SEAS"
+fn=$(basename "$f")
+en="${fn##*.}"
+fn="${fn%.*}"
+echo "[$fn].[$en]  [$f]"
+rsync -av --progress "$f" 'nvas@eniac.seas.upenn.edu:~/print'
+#TODO: Add here-string
+ssh nvas@eniac.seas.upenn.edu "cd print; if [[ \"$en\" == \"pdf\" ]];
+then pdf2ps \"${fn}.pdf\" \"${fn}.ps\"; lpr -P4alcove \"${fn}.ps\"; else lpr -P4alcove \"${fn}.${en}\"; fi; " 
+echo "$f printed!"
