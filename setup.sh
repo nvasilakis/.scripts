982d65dc29f0a5447cdfed69c7075f23cddcc5b7
Nikos Vasilakis <nikos.ailo@gmail.com>
2014-03-19 16:10:52 -0400

    Introducing a cross-distro install script

--- setup.sh
+++ setup.sh
@@ -1,90 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 
-##
-# 2012, Nikos Vasilakis
-# n.c.vasilakis@gmail.com
-#
-# Script that installs all required git repositories on the local
-# machine, registers submodules and setups .*rc files as soft links.
-#
-# Usage: ./setup.sh
-#
-# TODO:  
-#       *  Maybe install extra packages
-#       *  in case of ssh, grab setup keys, generate keys, wait for
-#       github and then continue
-##
+SETUP_LOC="https://raw.github.com/nvasilakis/scripts/master/post-setup.sh"
 
-clear
-# Function used for debugging output.
-function e {
-  echo $2 ".. $1";
-}
-function icheck {
-  sudo apt-get install "$1" || e "Install $1";
+isInstalled() {
+  [ -x "$(which $1)" ]
 }
 
-function install {
-  for prog; do
-    #echo "Install ${prog}"
-    command -v "${prog}" >/dev/null 2>&1 || { icheck "${prog}"; }
-  done
-}
+if isInstalled apt-get ; then 
+  echo "apt-get"
+  export PKG_MGR=1;
+  sudo apt-get install curl
 
-current_dir=$(pwd)
-# last two are for compiling gnu screen on debial-like systems
-install zsh screen git htop autoconf libncurses5-dev
-
-e "Clone via https(1) or ssh(2)? [1]> " -n
-read method;
-if [[ $method == 2 ]]; then
-  wget https://raw.github.com/nvasilakis/scripts/master/setup-keys.sh
-  chmod +x setup-keys.sh
-  ./setup-keys.sh
-  # cleanup
-  rm setup-keys.sh
-
-  git clone git@github.com:nvasilakis/immateriia.git ~/.vim
-  git clone git@github.com:nvasilakis/scripts.git ~/scripts
-  git clone git@github.com:nvasilakis/.emacs.d.git ~/.emacs.d
-  git clone git@github.com:nvasilakis/dotrc.git ~/.dotrc
-  cd ~/.vim
-  e 'updating submodules'
-  git submodule update --init
+elif isInstalled yum ; then 
+  echo "yum"
+  export PKG_MGR=2;
+elif isInstalled pacman ; then 
+  echo "pacman"
+  export PKG_MGR=3;
+elif isInstalled emerge ; then 
+  echo "emerge"
+  export PKG_MGR=4;
+elif isInstalled zypp ; then 
+  echo "zypp"
+  export PKG_MGR=5;
 else
-  git clone https://github.com/nvasilakis/immateriia.git ~/.vim
-  git clone https://github.com/nvasilakis/scripts.git ~/scripts
-  git clone https://github.com/nvasilakis/.emacs.d.git ~/.emacs.d
-  git clone https://github.com/nvasilakis/dotrc.git ~/.dotrc
-  cd ~/.vim
-  e 'updating submodules'
-  git submodule update --init
+  echo 'No package manager found!'
+  exit 2
 fi
 
-cd ~/.dotrc
-# run sudo xrdb ~/.Xdefaults to complete installation for fluxbox
-FILES="$(echo .*rc) .ss .gitconfig .Xdefaults .emacs"
-for i in $FILES; do
-        e "installing: ~/.dotrc/$i ~/$i"
-        rm -rf ~/$i
-        ln -s ~/.dotrc/$i ~/$i
-done
-
-e "heading to $current_dir"; cd $current_dir
-e "Clean up ${0}"; rm  ${0}
-
-e "Fetching screen"
-git clone git://git.savannah.gnu.org/screen.git
-cd screen/src
-./autogen.sh
-./configure --enable-colors256
-make
-if [[ -f screen ]]; then
-  sudo mv screen $(dirname $(which screen))
-  e "Clean up screen"
-  cd ~
-  rm -rf screen/
-  screen
-else
-  e 'Problem installing screen?'
-fi
+curl $SETUP_LOC | bash
