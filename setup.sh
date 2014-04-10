7897b27676dadf47bb0825611c59c1e0d83f469d
Nikos Vasilakis <nikos.ailo@gmail.com>
2014-04-09 22:17:44 -0400

    [Minor] Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,28 +14,29 @@ cleanup() {
   rm "$FNAME" "$THIS"
 }
 
-if isInstalled apt-get ; then 
-  echo "Running apt-get.."
-  export PKG_MGR=1;
-  sudo apt-get install curl
-elif isInstalled yum ; then 
-  echo "yum"
-  export PKG_MGR=2;
-elif isInstalled pacman ; then 
-  echo "pacman"
-  export PKG_MGR=3;
-elif isInstalled emerge ; then 
-  echo "emerge"
-  export PKG_MGR=4;
-elif isInstalled zypp ; then 
-  echo "zypp"
-  export PKG_MGR=5;
-else
-  echo 'No package manager found!'
-  exit 2
-fi
+check_pkg() {
+  if isInstalled apt-get ; then 
+    echo "Running apt-get.."
+    export PKG_MGR=1;
+    sudo apt-get install curl
+  elif isInstalled yum ; then 
+    echo "yum"
+    export PKG_MGR=2;
+  elif isInstalled pacman ; then 
+    echo "pacman"
+    export PKG_MGR=3;
+  elif isInstalled emerge ; then 
+    echo "emerge"
+    export PKG_MGR=4;
+  elif isInstalled zypp ; then 
+    echo "zypp"
+    export PKG_MGR=5;
+  else
+    echo 'No package manager found!'
+    exit 2
+  fi
+}
 
-#curl $SETUP_LOC | bash
 wget $SETUP_LOC -O "${FNAME}" 
 chmod +x "${FNAME}"
 ./"${FNAME}" && cleanup
