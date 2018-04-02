acbc9b25b38704f0b549a3b539685539a518e064
Nikos Vasilakis <nikos@vasilak.is>
2018-04-02 18:20:49 -0400

    Make sure .ssh directory exists

--- newuser.sh
+++ newuser.sh
@@ -8,7 +8,7 @@ sudo adduser $NUSER
 sudo mkdir ~/../$NUSER/.ssh
 
 # This assumes you have id_rsa.pub -- maybe check?
-
+sudo mkdir ../$NUSER/.ssh
 sudo cp id_rsa.pub ~/../$NUSER/.ssh/authorized_keys
 sudo chmod 600 ~/../$NUSER/.ssh/authorized_keys
 sudo chown "${NUSER}":"${NUSER}" ~/../$NUSER/.ssh/authorized_keys
