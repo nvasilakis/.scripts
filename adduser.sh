cc549808d12815bdac64860e35a2a585facf2a74
Nikos Vasilakis <nikos@vasilak.is>
2019-12-07 12:23:42 -0500

    Update adduser

--- adduser.sh
+++ adduser.sh
@@ -2,8 +2,8 @@
 #
 # ./adduser.sh <name>
 #
-# It creates a new user and copies their public
-# key from ~/<name>.pub.
+# It creates a new user and copies their PK
+# Expects public key as ~/<username>.pub
 
 if [ "$#" -ne 1 ]; then
   echo "Try ./$0 <user>, assuming ~/<user>.pub exists"
@@ -18,11 +18,12 @@ then
   sudo adduser $u
   sudo mkdir /home/$u/.ssh
   sudo touch /home/$u/.ssh/authorized_keys
-  sudo mv ./$u.pub /home/$u/.ssh/authorized_keys
+  sudo mv ~/$u.pub /home/$u/.ssh/authorized_keys
   sudo chown -R $u:$u /home/$u/.ssh
   sudo chmod 700 /home/$u/.ssh
   sudo chmod 600 /home/$u/.ssh/authorized_keys
   sudo cat /home/$u/.ssh/authorized_keys
+  # echo "If expected, run 'rm ~/$u.pub'."
 else
   echo "There is no ~/$u.pub!"
   exit 0
