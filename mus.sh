9599e80bafe8782d36d12443cec3e84fcc73b5b9
Nikos Vasilakis <nikos@vasilak.is>
2020-04-20 20:31:00 -0400

    Update screen script

--- mus.sh
+++ mus.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Multiuser screen
-
-name=${1:-dish}
+# ./mus.sh <session-name> <user>
+if [ "$#" -eq 2 ]; then
+  echo "./$0 <session-name> <user>"
+else
 
 # if /var/run/screen not 755
 # sudo chmod 755 /var/run/screen
 # sudo rm -fr /var/run/screen/*
 
-screen -dmS $name
-screen -S $name -X multiuser on
-# screen -S $name -X acladd bob
-screen -r $name
+screen -d -m -S $1
+screen -S $1 -X multiuser on
+screen -S $name -X acladd $2
+echo "screen -x $(whoami)/$i"
